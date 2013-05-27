2fb20d736760bfa9a25656c10ab722ec165bb936
Melvyn Laïly <mlaily@users.noreply.github.com>
2013-05-26 21:55:53 -0400

    mise en place d'un systeme pour g?rer plusieurs impl?mentations des joueurs simplement. d?but d'impl?mentation d'une logique de jeu (? revoir) classe de lock qui permet d'attendre le tour d'un joueur humain ou artificel.

---
+++ JeuDuMoulin/Game.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace JeuDuMoulin
+{
+	public class Game
+	{
+		private bool cancelAll = false;
+
+		public Graph Board { get; private set; }
+		public int Turn { get; private set; }
+		public Phase Phase { get; private set; }
+		public IPlayer Player1 { get; private set; }
+		public IPlayer Player2 { get; private set; }
+
+		public Game(IPlayer player1, IPlayer player2)
+		{
+			Board = new Graph();
+			Player1 = player1;
+			Player2 = player2;
+			Player1.Initialize(this);
+			Player2.Initialize(this);
+			Phase = JeuDuMoulin.Phase.First;
+			Turn = 1;
+		}
+
+		/// <summary>
+		/// à utiliser pour libérer les boucles bloquantes quand on quitte le programme
+		/// </summary>
+		public void Abort()
+		{
+			cancelAll = true;
+		}
+
+		public void Start()
+		{
+			System.Threading.Thread t = new Thread(GameLoop);
+			t.Name = "Game Loop";
+			t.Start();
+		}
+
+		private void GameLoop()
+		{
+			while (!cancelAll && Player1.PawnCount < 9 && Player2.PawnCount < 9)
+			{
+				using (var l = new Lock("Waiting for Player 1...", "Player 1 ended their turn!"))
+				{
+					Player1.Play(l);
+				}
+				using (var l = new Lock("Waiting for Player 2...", "Player 2 ended their turn!"))
+				{
+					Player2.Play(l);
+				}
+			}
+			Phase = JeuDuMoulin.Phase.Second;
+		}
+
+	}
+
+	public enum Phase
+	{
+		/// <summary>
+		/// placement
+		/// </summary>
+		First,
+		/// <summary>
+		/// déplacement
+		/// </summary>
+		Second,
+	}
+
+	public class GameRuleBrokenException : Exception
+	{
+		public GameRuleBrokenException(string message, Exception inner = null) : base(message, inner) { }
+	}
+}
--- JeuDuMoulin/Plateau.cs
+++ JeuDuMoulin/Plateau.cs
@@ -6,49 +6,81 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace JeuDuMoulin
 {
-	public partial class Plateau : UserControl
+	public partial class Plateau : UserControl, IPlayer
 	{
 
-		Point Origin;
-		const int SpacingCoef = 50;
-		Graph graph = new Graph();
-		Node SelectedNode;
+		public Game Game { get; private set; }
+		public int PawnCount { get; set; }
 
+		private Lock currentLock = new Lock(); //avoid NRE
+
+		#region Graphic representation
+		Point Origin;
+		const int SpacingCoef = 65;
+		Node SelectedNode;
 		const int PieRadius = 10;
 		Color background = Color.White;
 		Pen defaultPen = new Pen(Color.Black, 2);
 		Pen selectionPen = new Pen(Color.Yellow, 2);
 		Brush player1Brush = new SolidBrush(Color.Orange);
 		Brush player2Brush = new SolidBrush(Color.Cyan);
+		#endregion
+
+		public event EventHandler GraphicRefresh;
+
+		private void OnGraphicRefresh()
+		{
+			var handler = GraphicRefresh;
+			if (handler != null)
+			{
+				handler(this, EventArgs.Empty);
+			}
+		}
+
+		public void Initialize(Game game)
+		{
+			this.Game = game;
+			this.Invalidate();
+
+			//graphic debug
+			Random r = new Random(42);
+			foreach (var node in Game.Board.Nodes)
+			{
+				node.Occupation = (Occupation)r.Next(3);
+			}
+			SelectedNode = Game.Board.Nodes.ElementAt(3);
+		}
 
 		public Plateau()
 		{
 			InitializeComponent();
-			Origin = new Point(this.Width / 2, this.Height / 2);
-			Random r = new Random(42);
-			foreach (var node in graph.Nodes)
-			{
-				node.Occupation = (Occupation)r.Next(3);
-			}
-			SelectedNode = graph.Nodes.ElementAt(3);
 		}
 
 		private void Plateau_Paint(object sender, PaintEventArgs e)
 		{
-			DrawPlateau(e.Graphics);
+			if (this.Game == null)
+			{
+				//not yet initialized
+				e.Graphics.Clear(Color.Purple);
+			}
+			else
+			{
+				DrawPlateau(e.Graphics);
+			}
 		}
 
 		private void DrawPlateau(Graphics g)
 		{
 			g.Clear(background);
-			foreach (var segment in graph.GetAllEdges(Origin, SpacingCoef))
+			foreach (var segment in Game.Board.GetAllEdges(Origin, SpacingCoef))
 			{
 				g.DrawLine(defaultPen, segment.Item1, segment.Item2);
 			}
-			foreach (var node in graph.Nodes)
+			foreach (var node in Game.Board.Nodes)
 			{
 				Point nodeCenter = node.GetAbsoluteLocation(Origin, SpacingCoef);
 				switch (node.Occupation)
@@ -80,7 +112,7 @@ namespace JeuDuMoulin
 
 		private void Plateau_MouseClick(object sender, MouseEventArgs e)
 		{
-			var clickedNode = graph.MapPointToNode(e.Location, Origin, SpacingCoef);
+			var clickedNode = Game.Board.MapPointToNode(e.Location, Origin, SpacingCoef);
 			if (clickedNode != null)
 			{
 #if DEBUG
@@ -96,6 +128,7 @@ namespace JeuDuMoulin
 							//todo: check if the current pawn belongs to the appropriate player...
 							SelectedNode = clickedNode;
 							this.Invalidate();
+							OnGraphicRefresh();
 						}
 					}
 				}
@@ -106,10 +139,12 @@ namespace JeuDuMoulin
 						clickedNode.Occupation == Occupation.None &&
 						SelectedNode != clickedNode)
 					{
-						if (graph.MovePawn(SelectedNode, clickedNode, SelectedNode.Occupation))
+						if (Game.Board.MovePawn(SelectedNode, clickedNode, SelectedNode.Occupation))
 						{
 							SelectedNode = clickedNode;
 							this.Invalidate();
+							OnGraphicRefresh();
+							currentLock.Release();
 						}
 					}
 				}
@@ -121,6 +156,18 @@ namespace JeuDuMoulin
 #endif
 			}
 		}
+
+		private void Plateau_Load(object sender, EventArgs e)
+		{
+			Origin = new Point(this.Width / 2, this.Height / 2);
+		}
+
+		public void Play(Lock l)
+		{
+			//start the turn for this player
+			currentLock = l;
+		}
+
 	}
 
 }
