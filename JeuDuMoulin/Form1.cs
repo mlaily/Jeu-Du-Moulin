59398c399f44d6a11c60a9e76b733863d4e98d3b
Melvyn Laïly <mlaily@users.noreply.github.com>
2013-06-06 12:57:50 -0400

    bug d'appel cross thread

--- JeuDuMoulin/Form1.cs
+++ JeuDuMoulin/Form1.cs
@@ -28,17 +28,17 @@ namespace JeuDuMoulin
 			var plateau1 = new Human() { Location = new Point(12, 42), Size = new Size(500, 500) };
 			this.Controls.Add(plateau1);
 			//700 42
-			var plateau2 = new Human() { Location = new Point(700, 42), Size = new Size(500, 500) };
+			var plateau2 = new Plateau() { Location = new Point(700, 42), Size = new Size(500, 500) };
 			this.Controls.Add(plateau2);
 			//plateau1.GraphicRefresh += (o, e) => plateau2.Invalidate();
 			//plateau2.GraphicRefresh += (o, e) => plateau1.Invalidate();
 			player1 = plateau1; //new RandomAI("AI1");
-			player2 = plateau2; //new RandomAI("AI2");
+			player2 = new RandomAI("AI2");
 			game = new Game(player1, player2);
 			game.ArtificialWait = TimeSpan.FromMilliseconds(200);
-			//plateau2.Game = game;
-			//plateau2.Player = player2;
-			//plateau2.Invalidate();
+			plateau2.Game = game;
+			plateau2.Player = player2;
+			plateau2.Invalidate();
 			game.TurnEnded += (o, e2) => plateau1.Invalidate();
 			game.TurnEnded += (o, e2) => plateau2.Invalidate();
 			game.Start();
