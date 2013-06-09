250e962b503b7a7619709ea21bf64695856ec891
Melvyn Laïly <mlaily@users.noreply.github.com>
2013-06-09 18:11:09 -0400

    pointless refactoring (is it?)

--- JeuDuMoulin/Players/MinMaxAI.cs
+++ JeuDuMoulin/Players/MinMaxAI.cs
@@ -15,10 +15,9 @@ namespace JeuDuMoulin
 
 		public string Name { get; private set; }
 
-		Dictionary<int, Node> BoardCopy = new Dictionary<int, Node>();
+		private GameCopy GameCopy;
 
 		Node OpponentToRemoveReminder = null;
-
 
 		public MinMaxAI(string name)
 		{
@@ -29,174 +28,13 @@ namespace JeuDuMoulin
 		{
 			this.Game = game;
 			this.Control = new Game.PlayerControl(this);
-			var newBoard = Graph.CreateGraph();
-			foreach (var item in newBoard)
-			{
-				BoardCopy.Add(item.Id, item);
-			}
-		}
-
-		private void CloneBoardState()
-		{
-			foreach (var item in Game.Board)
-			{
-				BoardCopy[item.Id].Owner = item.Owner;
-			}
-		}
-
-		private void ApplyMoveToBoard(Move move)
-		{
-			if (move.Origin != null)
-			{
-				BoardCopy[move.Origin.Id].Owner = null;
-			}
-			if (move.Destination != null)
-			{
-				BoardCopy[move.Destination.Id].Owner = move.Player;
-			}
-			if (move.Removal != null)
-			{
-				BoardCopy[move.Removal.Id].Owner = null;
-			}
-		}
-
-		private void CancelMoveFromBoard(Move move)
-		{
-			if (move.Origin != null)
-			{
-				BoardCopy[move.Origin.Id].Owner = move.Player;
-			}
-			if (move.Destination != null)
-			{
-				BoardCopy[move.Destination.Id].Owner = null;
-			}
-			if (move.Removal != null)
-			{
-				BoardCopy[move.Removal.Id].Owner = move.Opponent;
-			}
-		}
-
-		private List<Move> FindAllMovesForCurrentBoard(IPlayer playing, IPlayer opponent)
-		{
-			List<Move> availableMoves = new List<Move>();
-			var opponentNodes = BoardCopy.Values.Where(x => x.Owner == opponent).ToList();
-
-			//possibilités: une par position libre, plus toutes les suppressions de l'adversaire possibles si la position fait un moulin
-			var availableNodes = BoardCopy.Values.Where(x => x.Owner == null);
-			foreach (var node in availableNodes)
-			{
-				if (Graph.IsCreatingAMill(node, playing))
-				{
-					foreach (var opponentNode in opponentNodes)
-					{
-						availableMoves.Add(new Move(playing, opponent) { Destination = node, Removal = opponentNode });
-					}
-				}
-				else
-				{
-					availableMoves.Add(new Move(playing, opponent) { Destination = node });
-				}
-			}
-			return availableMoves;
-		}
-
-		private Move MiniMax(IPlayer playing, IPlayer opponent, Move move = null, int depth = 0)
-		{
-			const int MAX_DEPTH = 3;
-			if (move != null) ApplyMoveToBoard(move);
-			if (depth == MAX_DEPTH)
-			{
-				int valuation = EvaluateBoardState();
-				move.Valuation = valuation;
-				CancelMoveFromBoard(move);
-				return move;
-			}
-			else
-			{
-				if (depth % 2 == 0)
-				{
-					//playing => max
-					var children = FindAllMovesForCurrentBoard(playing, opponent);
-					Move max = null;
-					foreach (var item in children)
-					{
-						var valuation = MiniMax(opponent, playing, item, depth + 1);
-						if (max == null || valuation.Valuation > max.Valuation)
-						{
-							max = item;
-						}
-					}
-					if (move != null) CancelMoveFromBoard(move);
-					if (move != null) move.Valuation = max.Valuation;
-					return max;
-				}
-				else
-				{
-					//opponent => min
-					var children = FindAllMovesForCurrentBoard(playing, opponent);
-					Move min = null;
-					foreach (var item in children)
-					{
-						var valuation = MiniMax(opponent, playing, item, depth + 1);
-						if (min == null || valuation.Valuation < min.Valuation)
-						{
-							min = item;
-						}
-					}
-					if (move != null) CancelMoveFromBoard(move);
-					if (move != null) move.Valuation = min.Valuation;
-					return min;
-				}
-			}
-		}
-
-		private int EvaluateBoardState()
-		{
-			int result = 0;
-			//each owned node adds 1
-			result += (int)Math.Round(1.0 * BoardCopy.Values.Count(x => x.Owner == this));
-			//each node owned by the opponent removes 1
-			result -= (int)Math.Round(1.0 * BoardCopy.Values.Count(x => x.Owner != null && x.Owner != this));
-			//each possible move add 1
-			result += (int)Math.Round(0.25 * FindAllMovesForCurrentBoard(this, this.Control.Opponent).Count);
-			//+1 for each mill -1 for each opponent mill
-			int count1;
-			int count2;
-			CountPlayersMills(this, this.Control.Opponent, out count1, out count2);
-			result += (int)Math.Round(2.0 * count1);
-			result -= (int)Math.Round(2.0 * count2);
-			return result;
-		}
-
-		private void CountPlayersMills(IPlayer player1, IPlayer player2, out int count1, out int count2)
-		{
-			//TODO optimiser pour éviter de bruteforce 3 fois de suites
-			count1 = 0;
-			count2 = 0;
-			foreach (var node in BoardCopy.Values.Where(x => x.Owner == player1))
-			{
-				if (Graph.IsCreatingAMill(node, player1))
-				{
-					count1++;
-				}
-			}
-			foreach (var node in BoardCopy.Values.Where(x => x.Owner == player2))
-			{
-				if (Graph.IsCreatingAMill(node, player2))
-				{
-					count2++;
-				}
-			}
-			//la fonction n'est pas optimale et on bruteforce toutes les cases,
-			//donc on compte 3 fois les moulins
-			count1 = (int)Math.Round(count1 / 3.0);
-			count2 = (int)Math.Round(count2 / 3.0);
+			GameCopy = new GameCopy(this, this.Control.Opponent);
 		}
 
 		public void PlacePawn(Guid token)
 		{
-			CloneBoardState();
-			var move = MiniMax(this, Control.Opponent);
+			GameCopy.SynchronizeBoardState(Game);
+			var move = GameCopy.MiniMax(this, Control.Opponent);
 			if (move.Removal != null)
 			{
 				OpponentToRemoveReminder = Game.Board.First(x => x.Id == move.Removal.Id);
@@ -237,32 +75,215 @@ namespace JeuDuMoulin
 			return this.Name;
 		}
 
-		class Move
+	}
+
+	class Move
+	{
+		public IPlayer Player { get; set; }
+		public IPlayer Opponent { get; set; }
+		public Node Origin { get; set; }
+		public Node Destination { get; set; }
+		public Node Removal { get; set; }
+
+		public int Valuation { get; set; }
+
+		public Move(IPlayer player, IPlayer opponent)
 		{
-			public IPlayer Player { get; set; }
-			public IPlayer Opponent { get; set; }
-			public Node Origin { get; set; }
-			public Node Destination { get; set; }
-			public Node Removal { get; set; }
+			this.Player = player;
+			this.Opponent = opponent;
+		}
 
-			public int Valuation { get; set; }
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendFormat("{0}|", Valuation);
+			sb.Append(Player);
+			sb.Append(": ");
+			if (Origin != null) sb.AppendFormat("From {0} ", Origin);
+			if (Destination != null) sb.AppendFormat("To {0} ", Destination);
+			if (Removal != null) sb.AppendFormat("+Remove {0}", Removal);
+			return sb.ToString();
+		}
+	}
 
-			public Move(IPlayer player, IPlayer opponent)
+	class GameCopy
+	{
+		public Dictionary<int, Node> Board = new Dictionary<int, Node>();
+		public IPlayer Player { get; set; }
+		public IPlayer Opponent { get; set; }
+
+		public Phase phase;
+		public int pawnsToPlace;
+		public int opponentPawnsToPlace;
+
+		public GameCopy(IPlayer player, IPlayer opponent)
+		{
+			this.Player = player;
+			this.Opponent = opponent;
+			var newBoard = Graph.CreateGraph();
+			foreach (var item in newBoard)
 			{
-				this.Player = player;
-				this.Opponent = opponent;
+				Board.Add(item.Id, item);
 			}
+		}
 
-			public override string ToString()
+		public void SynchronizeBoardState(Game game)
+		{
+			foreach (var item in game.Board)
 			{
-				StringBuilder sb = new StringBuilder();
-				sb.AppendFormat("{0}|", Valuation);
-				sb.Append(Player);
-				sb.Append(": ");
-				if (Origin != null) sb.AppendFormat("From {0} ", Origin);
-				if (Destination != null) sb.AppendFormat("To {0} ", Destination);
-				if (Removal != null) sb.AppendFormat("+Remove {0}", Removal);
-				return sb.ToString();
+				Board[item.Id].Owner = item.Owner;
+			}
+			phase = game.Phase;
+			pawnsToPlace = Player.Control.PawnsToPlace;
+			opponentPawnsToPlace = Opponent.Control.PawnsToPlace;
+		}
+
+		private void ApplyMove(Move move)
+		{
+			if (move.Origin != null)
+			{
+				Board[move.Origin.Id].Owner = null;
+			}
+			if (move.Destination != null)
+			{
+				Board[move.Destination.Id].Owner = move.Player;
+			}
+			if (move.Removal != null)
+			{
+				Board[move.Removal.Id].Owner = null;
+			}
+		}
+
+		private void CancelMove(Move move)
+		{
+			if (move.Origin != null)
+			{
+				Board[move.Origin.Id].Owner = move.Player;
+			}
+			if (move.Destination != null)
+			{
+				Board[move.Destination.Id].Owner = null;
+			}
+			if (move.Removal != null)
+			{
+				Board[move.Removal.Id].Owner = move.Opponent;
+			}
+		}
+
+		private List<Move> FindPossibleMoves(IPlayer playing, IPlayer opponent)
+		{
+			List<Move> availableMoves = new List<Move>();
+			var opponentNodes = Board.Values.Where(x => x.Owner == opponent).ToList();
+
+			//possibilités: une par position libre, plus toutes les suppressions de l'adversaire possibles si la position fait un moulin
+			var availableNodes = Board.Values.Where(x => x.Owner == null);
+			foreach (var node in availableNodes)
+			{
+				if (Graph.IsCreatingAMill(node, playing))
+				{
+					foreach (var opponentNode in opponentNodes)
+					{
+						availableMoves.Add(new Move(playing, opponent) { Destination = node, Removal = opponentNode });
+					}
+				}
+				else
+				{
+					availableMoves.Add(new Move(playing, opponent) { Destination = node });
+				}
+			}
+			return availableMoves;
+		}
+
+		private int EvaluateBoardState()
+		{
+			int result = 0;
+			//each owned node adds 1
+			result += (int)Math.Round(1.0 * Board.Values.Count(x => x.Owner == this));
+			//each node owned by the opponent removes 1
+			result -= (int)Math.Round(1.0 * Board.Values.Count(x => x.Owner != null && x.Owner != this));
+			//each possible move add 1
+			result += (int)Math.Round(0.25 * FindPossibleMoves(Player, Opponent).Count);
+			//+1 for each mill -1 for each opponent mill
+			int count1;
+			int count2;
+			CountPlayersMills(Player, Opponent, out count1, out count2);
+			result += (int)Math.Round(2.0 * count1);
+			result -= (int)Math.Round(2.0 * count2);
+			return result;
+		}
+
+		private void CountPlayersMills(IPlayer player1, IPlayer player2, out int count1, out int count2)
+		{
+			//TODO optimiser pour éviter de bruteforce 3 fois de suites
+			count1 = 0;
+			count2 = 0;
+			foreach (var node in Board.Values.Where(x => x.Owner == player1))
+			{
+				if (Graph.IsCreatingAMill(node, player1))
+				{
+					count1++;
+				}
+			}
+			foreach (var node in Board.Values.Where(x => x.Owner == player2))
+			{
+				if (Graph.IsCreatingAMill(node, player2))
+				{
+					count2++;
+				}
+			}
+			//la fonction n'est pas optimale et on bruteforce toutes les cases,
+			//donc on compte 3 fois les moulins
+			count1 = (int)Math.Round(count1 / 3.0);
+			count2 = (int)Math.Round(count2 / 3.0);
+		}
+
+		public Move MiniMax(IPlayer playing, IPlayer opponent, Move move = null, int depth = 0)
+		{
+			const int MAX_DEPTH = 3;
+			if (move != null) ApplyMove(move);
+			if (depth == MAX_DEPTH)
+			{
+				int valuation = EvaluateBoardState();
+				move.Valuation = valuation;
+				CancelMove(move);
+				return move;
+			}
+			else
+			{
+				if (depth % 2 == 0)
+				{
+					//playing => max
+					var children = FindPossibleMoves(playing, opponent);
+					Move max = null;
+					foreach (var item in children)
+					{
+						var valuation = MiniMax(opponent, playing, item, depth + 1);
+						if (max == null || valuation.Valuation > max.Valuation)
+						{
+							max = item;
+						}
+					}
+					if (move != null) CancelMove(move);
+					if (move != null) move.Valuation = max.Valuation;
+					return max;
+				}
+				else
+				{
+					//opponent => min
+					var children = FindPossibleMoves(playing, opponent);
+					Move min = null;
+					foreach (var item in children)
+					{
+						var valuation = MiniMax(opponent, playing, item, depth + 1);
+						if (min == null || valuation.Valuation < min.Valuation)
+						{
+							min = item;
+						}
+					}
+					if (move != null) CancelMove(move);
+					if (move != null) move.Valuation = min.Valuation;
+					return min;
+				}
 			}
 		}
 
